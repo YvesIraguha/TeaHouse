84ccb096d824aa9173d6225ea356a8b376826168
YvesIraguha <iraguhaivos@gmail.com>
2019-11-09 14:51:01 +0200

    Set up devlopment environment - set-up eslint, prettier and babel - set up testing environment - setup travis - add travis badge - Set pre-commit-hooks
    [#169663247]

---
+++ app.js
@@ -0,0 +1,32 @@
+import createError from "http-errors";
+import express from "express";
+import logger from "morgan";
+
+import indexRouter from "./routes/index";
+import usersRouter from "./routes/admin";
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/", indexRouter);
+app.use("/admin", usersRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send({ message: "error is happening over" });
+});
+
+export default app;
