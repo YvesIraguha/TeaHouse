9a938c1e1f1b99adcde7ff26472fa36ebce9ee98
YvesIraguha <iraguhaivos@gmail.com>
2020-08-11 11:54:45 +0200

    add new categories - add interviews - add lit news - add gossips

--- src/middlewarres/handleAsync.js
+++ src/middlewarres/handleAsync.js
@@ -1,7 +1,8 @@
-const asyncHandler = cb => async (req, res, next) => {
+const asyncHandler = (cb) => async (req, res, next) => {
   try {
     await cb(req, res, next);
   } catch (error) {
+    console.log(error);
     return res.status(500).send({ error: "Something went wrong on the server" });
   }
 };
