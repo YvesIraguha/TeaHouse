8159c16b1cbff89b79302fa8faafc1d12356da7f
YvesIraguha <iraguhaivos@gmail.com>
2020-08-31 17:46:34 +0200

    chore(cors): add SSL certificate

--- src/middlewarres/handleAsync.js
+++ src/middlewarres/handleAsync.js
@@ -2,7 +2,6 @@ const asyncHandler = (cb) => async (req, res, next) => {
   try {
     await cb(req, res, next);
   } catch (error) {
-    console.log(error);
     return res.status(500).send({ error: "Something went wrong on the server" });
   }
 };
