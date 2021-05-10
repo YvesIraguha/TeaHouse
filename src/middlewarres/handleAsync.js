8446a795598ef73853f559f5679273b0dd7b26d1
YvesIraguha <iraguhaivos@gmail.com>
2021-05-10 11:13:52 +0200

    refactor interal server error response

--- src/middlewarres/handleAsync.js
+++ src/middlewarres/handleAsync.js
@@ -2,7 +2,7 @@ const asyncHandler = (cb) => async (req, res, next) => {
   try {
     await cb(req, res, next);
   } catch (error) {
-    return res.status(500).send({ error: "Something went wrong on the server" });
+    return res.status(500).send({ error: error.message });
   }
 };
 
